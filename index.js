7c6407a306edda92c5db5a225099ba737eb06836
VIKASH KUMAR SINHA <74089561+vikashsinha019@users.noreply.github.com>
2021-07-28 20:28:20 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+var number = Math.floor(Math.random() * 4) + 1;
+
+var c = "images/image" + number + ".jpg";
+document.querySelector("img").setAttribute("src", c);
+
+
+
+
+
+// prompt("Enter Your Name: ");
+// prompt("Enter Their Name: ");
+// var number = Math.floor(Math.random() * 100) + 1;
+
+// if(number >= 70){
+//     alert("your love score is : " + number + " %" + " Both you are perfect for each other");
+// }
+// else if(number < 70){
+//     alert("your love score is : " + number + " %" + " You become nice friends, but not love partner");
+// }
+
+
